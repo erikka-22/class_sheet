98757eabe2e62c6de325fb3a2b9718702fc9b0a0
岩崎絵里加 <iwasakierika@iwasakierikanoMacBook-Air.local>
2021-04-11 18:39:36 +0900

    名前を表示させた

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Data from './data/person.json';
 
-function Square(props) {
+function Sheet(props) {
   return (
-    <button 
-      className="square"
-      onClick={props.onClick}
-    >
+    <div className="sheet">
       {props.value}
-    </button>
+    </div>
   );
 }
 
 class Board extends React.Component {
 
-  renderSquare(i) {
+  renderSheet(i) {
     return (
-      <Square 
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)} 
+      <Sheet 
+        value={this.props.sheets[i]}
+        onClick={() => this.props.onClick()} 
       />
     );
   }
@@ -28,97 +26,68 @@ class Board extends React.Component {
     return (
       <div>
         <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+          {this.renderSheet(0)}
+          {this.renderSheet(1)}
+          {this.renderSheet(2)}
+          {this.renderSheet(3)}
+          {this.renderSheet(4)}
         </div>
         <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
+          {this.renderSheet(5)}
+          {this.renderSheet(6)}
+          {this.renderSheet(7)}
+          {this.renderSheet(8)}
+          {this.renderSheet(9)}
         </div>
         <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+          {this.renderSheet(10)}
+          {this.renderSheet(11)}
+          {this.renderSheet(12)}
+          {this.renderSheet(13)}
+          {this.renderSheet(14)}
+        </div>
+        <div className="board-row">
+          {this.renderSheet(15)}
+          {this.renderSheet(16)}
+          {this.renderSheet(17)}
+          {this.renderSheet(18)}
+          {this.renderSheet(19)}
         </div>
       </div>
     );
   }
 }
 
-class Game extends React.Component {
+class Classroom extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      history: [{
-        squares: Array(9).fill(null)
-      }],
-      stepNumber: 0,
-      xIsNext: true
+      sheets: Array(20).fill(null) 
     }
   }
 
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    
-    if (calculateWinner(squares) || squares[i]) {
-      return;
+  handleClick() {
+    const sheets = this.state.sheets;
+    const students = Data.class.students;
+    for (let i = 0; i < 20; i++) {
+      if (i % 2 === 1) {
+        sheets[i] = students[Math.floor(Math.random() * students.length)]["name"];
+      }
     }
-
-    squares[i] = this.state.xIsNext ? 'X' : '○';
     this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: (step % 2) === 0,
+      sheets: sheets
     });
   }
 
   render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
-    const moves = history.map((step, move) => {
-      const desc = move ? 
-        'Go to move #' + move :
-        'Go to game start';
-      return (
-        <li key={move}> 
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
-        </li>
-      )
-    })
-
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : '○');
-    }
-
+    const shuffle = '席替え';
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className="classroom">
+        <div className="classroom-board">
           <Board 
-            squares={current.squares}
-            onClick={i => this.handleClick(i)}
+            sheets={this.state.sheets}
           />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+          <button onClick={() => this.handleClick()}>{shuffle}</button>
         </div>
       </div>
     );
@@ -128,26 +97,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Classroom />,
   document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
